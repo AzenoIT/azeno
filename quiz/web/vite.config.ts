6bfa97275e4ad755bdd83e5d39034418a2f50e1d
Krzysiek <krzysztof.plonka64@gmail.com>
2023-03-07 18:30:15 +0100

    AQ-34 Connected eslint with Vite

--- quiz/web/vite.config.ts
+++ quiz/web/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import eslint from "vite-eslint-plugin";
 
 export default defineConfig({
-  server: {
-    port: parseInt(process.env.VITE_PORT),
-    host: "0.0.0.0",
-  },
-  plugins: [react()],
-})
+    server: {
+        port: parseInt(process.env.VITE_PORT),
+        host: "0.0.0.0",
+    },
+    plugins: [react(), eslint({ fix: true })],
+});
