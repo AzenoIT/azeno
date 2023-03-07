36b62c4bd80bb482e91d26da7736aafa7ac2b1c0
Krzysiek <krzysztof.plonka64@gmail.com>
2023-03-07 09:11:48 +0100

    Updated Vite config to use env vars.

--- quiz/web/vite.config.ts
+++ quiz/web/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   server: {
-    port: 3000,
+    port: parseInt(process.env.VITE_PORT),
     host: "0.0.0.0",
   },
   plugins: [react()],
