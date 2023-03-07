0b27bdf598f01d1b23f4edc2aee260a37ed01e2d
Martyna Witkowska <mar.witkowska@gmail.com>
2023-03-07 14:56:52 +0100

    configure postgres database connection for django, add postgres db to docker compose

--- quiz/backend/config/settings.py
+++ quiz/backend/config/settings.py
@@ -13,6 +13,8 @@ import os
 
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,6 +82,13 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASE_URL = os.environ.get('DB_CONNECTION_STRING')
+db_from_env = dj_database_url.config(
+    default=DATABASE_URL, conn_max_age=500, ssl_require=False
+)
+
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
