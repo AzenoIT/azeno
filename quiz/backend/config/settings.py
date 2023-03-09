3e912546b223224beb4cb30dd2cfc6e731904e32
Martyna Witkowska <mar.witkowska@gmail.com>
2023-03-09 20:28:18 +0100

    add unified proxy service name, change assets url back to static url to avoid name collision with Vite React TS

--- quiz/backend/config/settings.py
+++ quiz/backend/config/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
