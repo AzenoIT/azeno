50ecb1b5e989a0dfce9d2b834edd1deb43050042
Krzysiek <krzysztof.plonka64@gmail.com>
2023-03-10 22:12:09 +0100

    AQ-46 Fixed TS erros.

--- quiz/web/src/modules/common/hooks/useApiClient.ts
+++ quiz/web/src/modules/common/hooks/useApiClient.ts
@@ -1,48 +1,19 @@
 import axios, { AxiosInstance, CreateAxiosDefaults } from "axios";
 import useJwtToken from "modules/common/hooks/useJwtToken";
 
-const isoDateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d*)?$/;
-
-function isIsoDateString(value: unknown): boolean {
-    return !!value && typeof value === "string" && isoDateFormat.test(value);
-}
-
-/**
- * Converts in place ISO datetime strings into a Date objects.
- */
-function handleDates(body: Record<string, unknown> | null | undefined | string | number | boolean) {
-    if (body === null || body === undefined || typeof body !== "object") {
-        return;
-    }
-
-    for (const key of Object.keys(body)) {
-        const value = body[key];
-        if (isIsoDateString(value)) {
-            body[key] = new Date(value as string);
-        } else if (typeof value === "object") {
-            handleDates(value as Record<string, unknown>);
-        }
-    }
-}
-
 /**
  * Hook allowing access to a better Axios, which:
  * - Fills Authorization header with Bearer token, if no Authorization header was provided.
- * - Parses all ISO datetime objects found in response to instance of Date.
  * */
 export default function useApiClient(): AxiosInstance {
     const [token] = useJwtToken();
     const client = axios.create({} satisfies CreateAxiosDefaults);
 
-    client.interceptors.response.use((originalResponse) => {
-        handleDates(originalResponse.data);
-        return originalResponse;
-    });
-
     client.interceptors.request.use((originalRequest) => {
-        originalRequest.headers = originalRequest.headers || {};
-        originalRequest.headers.Authorization = originalRequest.headers.Authorization || `Bearer ${token}`;
-        return originalRequest;
+        const newRequest = { ...originalRequest };
+        newRequest.headers = originalRequest.headers || {};
+        newRequest.headers.Authorization = originalRequest.headers.Authorization || `Bearer ${token}`;
+        return newRequest;
     });
 
     return client;
