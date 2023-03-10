5ca9ce2b943634d827eba031bfa549c4133f201a
Krzysiek <krzysztof.plonka64@gmail.com>
2023-03-10 14:06:00 +0100

    AQ-46 Configured support for absolute imports

--- quiz/web/vite.config.ts
+++ quiz/web/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import eslint from "vite-eslint-plugin";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
     server: {
         port: parseInt(process.env.VITE_PORT),
         host: "0.0.0.0",
     },
-    plugins: [react(), eslint({ fix: true })],
+    plugins: [
+        react(),
+        tsconfigPaths(),
+        { ...eslint({ failOnError: true, failOnWarning: false }), apply: "build" },
+        { ...eslint({ failOnError: false, failOnWarning: false, fix: true }), apply: "serve", enforce: "post" },
+    ],
 });
