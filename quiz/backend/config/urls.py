e5d95565806f83896889598dbcf607bc6479a3a4
MWasile <maaciej.wasilewski@gmail.com>
2023-03-11 21:55:55 +0100

    change poetry behavior on production server, now install packages without developer dependencies

--- quiz/backend/config/urls.py
+++ quiz/backend/config/urls.py
@@ -24,8 +24,11 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
+    try:
+        import debug_toolbar
+    except ImportError:
+        pass
+    else:
+        urlpatterns += [
+            path('__debug__/', include(debug_toolbar.urls)),
+        ]
