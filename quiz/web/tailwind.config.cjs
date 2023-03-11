d0ca45811422adf9c1fc3dc0ae769d187ec2ee54
olivergav <oliwier.gawlik0@gmail.com>
2023-03-11 02:57:11 +0100

    Resolved conversations change requests

--- quiz/web/tailwind.config.cjs
+++ quiz/web/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+    content: ["./src/**/*.{html,js,jsx,ts,tsx,ind}", "index.html"],
     theme: {
         extend: {},
     },
