98e428e2e710d850c8f5e5205f086d825074b1a2
olivergav <oliwier.gawlik0@gmail.com>
2023-03-11 15:53:29 +0100

    tailwind for vite config

--- quiz/web/tailwind.config.cjs
+++ quiz/web/tailwind.config.cjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js,jsx,ts,tsx,ind}", "index.html"],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
         extend: {},
     },
     plugins: [],
-};
+}
