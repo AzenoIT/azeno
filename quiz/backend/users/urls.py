83f1ff9c3efeaf835880b2018bb6d90cc2ba2073
Martyna Witkowska <mar.witkowska@gmail.com>
2023-03-12 11:02:03 +0100

    add register, login, logut endpoints for users

--- quiz/backend/users/urls.py
+++ quiz/backend/users/urls.py
@@ -8,4 +8,6 @@ app_name = 'users'
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.QuizCustomUserCreateView.as_view(), name='register'),
+    path('logout/', views.BlackListTokenUpdateView.as_view(), name='logout'),
 ]
