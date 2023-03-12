d0ebb7c69ca820d63babdd82b8a0c36f758f64af
Martyna Witkowska <mar.witkowska@gmail.com>
2023-03-12 10:08:03 +0100

    change users migrations, move token entpoints into users app, set reverse proxy for api endpoint, register user model in admin

--- quiz/backend/config/urls.py
+++ quiz/backend/config/urls.py
@@ -14,13 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path, include
 
 # TODO: change admin endpoint for production and reverse proxy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/users/', include('users.urls'))
 ]
--- quiz/backend/users/models.py
+++ quiz/backend/users/models.py
@@ -1,38 +1,9 @@
 import uuid
 
-from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-
-class QuizCustomUserManager(BaseUserManager):
-
-    use_in_migrations = True
-
-    def create_user(self, email, password, **kwargs):
-        if not email:
-            raise ValueError(_('Email field is required'))
-
-        email = self.normalize_email(email)
-        user = self.model(email=email, **kwargs)
-        user.is_active = False
-        user.set_password(password)
-        user.save()
-
-        return user
-
-    def create_superuser(self, email, password=None):
-        user = self.create_user(email=email, password=password)
-
-        user.is_admin = True
-        user.is_staff = True
-        user.is_active = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
-
 
 class QuizCustomUser(AbstractUser):
     id = models.UUIDField(default=uuid.uuid4(), primary_key=True, editable=False, db_index=True)
@@ -40,9 +11,7 @@ class QuizCustomUser(AbstractUser):
     username = models.CharField(max_length=100, validators=[UnicodeUsernameValidator()])
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-    objects = QuizCustomUserManager()
+    REQUIRED_FIELDS = ['email address']
 
     def __str__(self):
         return self.email
