42eea9b64f9c6f5d958e26d659dae5e2b095cdae
Martyna Witkowska <mar.witkowska@gmail.com>
2023-03-13 22:36:00 +0100

    remove hardcoded db name and if statement, leave port listening

--- quiz/backend/entrypoint.sh
+++ quiz/backend/entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-set -e
+echo "Waiting for postgres ..."
 
-if [ "$POSTGRES_DB" = "azeno_quiz" ]
-then
-  echo "Waiting for postgres ..."
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  sleep 0.1
+  done
 
-  while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-    sleep 0.1
-    done
+  echo "PostgreSQL started"
 
-    echo "PostgreSQL started"
-
-fi
 
 python manage.py collectstatic --no-input --clear
 python manage.py migrate
