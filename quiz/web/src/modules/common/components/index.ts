125c9d8074e55e15b515d5319add3f7ecc8d5149
Krzysiek <krzysztof.plonka64@gmail.com>
2023-03-15 13:00:09 +0100

    AQ-46 Added tests for ProtectedRoute.

---
+++ quiz/web/src/modules/common/components/index.ts
@@ -0,0 +1,4 @@
+import ErrorBoundary from "modules/common/components/ErrorBoundary";
+import ProtectedRoute from "modules/common/components/ProtectedRoute";
+
+export { ErrorBoundary, ProtectedRoute };
