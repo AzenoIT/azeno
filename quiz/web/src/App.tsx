b8b586c37692657d33fbcce33c9fff0ae6a5422d
Krzysiek <krzysztof.plonka64@gmail.com>
2023-03-29 15:21:52 +0200

    AQ-71 Removed AuthProvider.tsx

--- quiz/web/src/App.tsx
+++ quiz/web/src/App.tsx
@@ -1,20 +1,16 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { AuthConfig, AuthProvider } from "modules/common/Auth";
 import ErrorBoundary from "modules/common/components/ErrorBoundary";
 import { RouterProvider } from "react-router-dom";
 import { router } from "router";
 
 function App() {
     const queryClient = new QueryClient();
-    const authConfig: AuthConfig = { loginEndpoint: "/api/v1/token/", refreshEndpoint: "/api/v1/token/refresh/" };
 
     return (
         <QueryClientProvider client={queryClient}>
-            <AuthProvider config={authConfig}>
-                <ErrorBoundary>
-                    <RouterProvider router={router} />
-                </ErrorBoundary>
-            </AuthProvider>
+            <ErrorBoundary>
+                <RouterProvider router={router} />
+            </ErrorBoundary>
         </QueryClientProvider>
     );
 }
--- quiz/web/src/modules/common/Auth/index.ts
+++ quiz/web/src/modules/common/Auth/index.ts
@@ -1,5 +1,3 @@
-import { AuthProvider, useAuthConfig } from "modules/common/Auth/AuthProvider";
-import { type AuthConfig } from "modules/common/Auth/types";
 import useAuth from "modules/common/Auth/useAuth";
 
-export { AuthProvider, useAuth, useAuthConfig, type AuthConfig };
+export { useAuth };
