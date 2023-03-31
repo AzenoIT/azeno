17a2a868a2af63b05d50c736b3f3d8d748b5a543
MaryLangiewicz <maria.langiewicz@interia.pl>
2023-03-31 20:53:10 +0200

    added a full view of the start page

--- quiz/web/src/modules/common/components/Start/NavBar.tsx
+++ quiz/web/src/modules/common/components/Start/NavBar.tsx
@@ -6,7 +6,7 @@ import Typography from "@mui/material/Typography";
 
 function NavBar() {
     return (
-        <AppBar position="sticky">
+        <AppBar position="sticky" sx={{ bgcolor: "#4459A9" }}>
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
                     <img className="w-20 h-20" src={Logo} alt="logo" />
