39866334d3b5161791db4bcb32ece279c9a396fa
MaryLangiewicz <maria.langiewicz@interia.pl>
2023-03-31 14:12:41 +0200

    update routing and tailwind config

--- quiz/web/src/modules/common/components/index.ts
+++ quiz/web/src/modules/common/components/index.ts
@@ -1,4 +1,5 @@
 import ErrorBoundary from "modules/common/components/ErrorBoundary";
 import PlayerRequired from "modules/common/components/PlayerRequired";
+import Start from "modules/common/components/Start";
 
-export { ErrorBoundary, PlayerRequired };
+export { ErrorBoundary, PlayerRequired, Start };
--- quiz/web/tailwind.config.cjs
+++ quiz/web/tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        colors: {
+            primary: "#4459a9",
+        },
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        {
+            tailwindcss: { config: "./tailwindcss-config.js" },
+        },
+    ],
 };
