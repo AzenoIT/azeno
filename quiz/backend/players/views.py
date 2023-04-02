d475d04a1731d4aa40a675765641c180575b7a9a
MWasile <maaciej.wasilewski@gmail.com>
2023-04-02 07:47:13 +0200

    change endpoint name and number of returned nicknames

--- quiz/backend/players/views.py
+++ quiz/backend/players/views.py
@@ -9,17 +9,16 @@ from helpers.gen_username.generate import generate_username
 
 class NicknameGeneratorAPIView(APIView):
     """This view is used to suggest a nickname for a new player.
-    Pass query parameter `count` to specify the number of nicknames to generate, default is 1.
+    Return a list of 10 nicknames.
     Authentication is not required.
 
     :param count: Number of nicknames to generate.
     :type count: int
-    :returns: A list of nicknames.
+    :returns: List with 10 nicknames.
     :rtype: list[str]
     """
 
     def get(self, request) -> Response:
-        count: int = int(self.request.query_params.get("count", 1))
-        nicknames: List[str] = generate_username(count)
+        nicknames: List[str] = generate_username(10)
 
         return Response(nicknames, status=status.HTTP_200_OK)
