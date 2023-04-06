1d5f619b168043df810ff1f804b6d1187837fe85
Krzysztof Plonka <krzysztof.plonka64@gmail.com>
2023-04-06 23:32:48 +0200

    AQ-101 Example button story

--- bank/.storybook/main.ts
+++ bank/.storybook/main.ts
@@ -1,7 +1,20 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import * as path from "path";
+
 const config: StorybookConfig = {
-    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-    addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+    stories: ["../src/components/**/*.mdx", "../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+        {
+            name: "@storybook/addon-postcss",
+            options: {
+                cssLoaderOptions: { importLoaders: 1 },
+                postcssLoaderOptions: { implementation: require("postcss") },
+            },
+        },
+    ],
     framework: {
         name: "@storybook/react-vite",
         options: {},
