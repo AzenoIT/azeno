52b3a2bbd00e9e825995bd76fad771117bdb5bce
ToJestKrzysio <82170337+ToJestKrzysio@users.noreply.github.com>
2023-04-11 14:27:22 +0200

    Revert "AQ-101_Storybook"

--- bank/src/mocks/server.ts
+++
@@ -1,7 +0,0 @@
-import { setupServer } from "msw/node";
-import { RestHandler } from "msw";
-
-const handlers: RestHandler[] = [];
-
-// eslint-disable-next-line import/prefer-default-export
-export const server = setupServer(...handlers);
