60d284dfe13437061faedd8fa8ee3c4233a32669
Krzysiek <krzysztof.plonka64@gmail.com>
2023-04-12 23:32:25 +0200

    AQ-103 Fixed tailwind

--- quiz/web/tailwind.config.cjs
+++ quiz/web/tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "../../node_modules/@azeno/bank/**/*.{js,ts,jsx,tsx}"],
     important: "#root",
     theme: {
         extend: {
@@ -13,6 +13,11 @@ module.exports = {
                 primary70: "#92A7FD",
                 primary90: "#DDE1FF",
                 secondary90: "#DBE1FF",
+                primary: "#4459A9",
+                secondary: "#DBE1FF",
+                success: "#65A014",
+                warning: "#FDBC11",
+                error: "#BA1A1A",
             },
         },
     },
