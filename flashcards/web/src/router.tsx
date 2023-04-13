e24ca6d229f860c9d0015f78818ecb4ce3e9bd9d
Krzysiek K <krzysztof.kulka@hotmail.com>
2023-04-13 12:49:51 +0200

    basic routes

--- flashcards/web/src/router.tsx
+++ flashcards/web/src/router.tsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "pages/Dashboard";
+import GenericTestsAll from "pages/GenericTestsAll";
 
 // eslint-disable-next-line import/prefer-default-export
 export const router = createBrowserRouter([
@@ -9,5 +11,29 @@ export const router = createBrowserRouter([
     {
         path: "/login",
         element: <h1>Login</h1>,
+    },
+    {
+        path: "/add-new-deck",
+        element: <h1>Add new deck</h1>,
+    },
+    {
+        path: "/library",
+        element: <h1>Library</h1>,
+    },
+    {
+        path: "/statistics",
+        element: <h1>Statistics</h1>,
+    },
+    {
+        path: "/marketplace",
+        element: <h1>Marketplace</h1>,
+    },
+    {
+        path: "/tests",
+        element: <GenericTestsAll />,
+    },
+    {
+        path: "/dashboard",
+        element: <Dashboard />,
     },
 ]);
