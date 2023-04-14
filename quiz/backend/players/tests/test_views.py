fe3909252615b552a5adb834d77ef3a0e0ebb041
SlawCzech <slaw.czech@gmail.com>
2023-04-14 12:45:58 +0200

    fetch player profile data

--- quiz/backend/players/tests/test_views.py
+++ quiz/backend/players/tests/test_views.py
@@ -1,3 +1,5 @@
+import uuid
+
 from rest_framework import status
 from players.views import (
     NicknameGeneratorAPIView,
@@ -28,21 +30,19 @@ def test_player_view_create_player_save_in_db(player_api):
     assert PlayerCreateAPIView.queryset.get(uuid=response.data["uuid"])
 
 
-def test_retrieve_player_view_returns_data(player_api, api_rf):
-    player = player_api()
-    request = api_rf.get(f"/api/v1/players/{player.data['uuid']}/")
-    response = PlayerRetrieveAPIView.as_view()(request, uuid=player.data["uuid"])
+def test_retrieve_player_view_returns_data(profile_data, api_rf):
+    url = f"/api/v1/players/{profile_data.player.uuid}/"
+    request = api_rf.get(url, content_type="application/json")
+    response = PlayerRetrieveAPIView.as_view()(request, uuid=profile_data.player.uuid)
 
     assert response.status_code == 200
-    assert response.data["nick"] == player.data["nick"]
-    assert response.data["uuid"] == str(player.data["uuid"])
+    assert response.data["nick"] == profile_data.player.nick
+    assert response.data["uuid"] == str(profile_data.player.uuid)
 
 
-def test_retrieve_player_view_with_wrong_uuid_returns_404(player_api, api_rf):
-    player = player_api()
-    request = api_rf.get(f"/api/v1/players/{player.data['uuid']}/")
-    response = PlayerRetrieveAPIView.as_view()(
-        request, uuid=f'{player.data["uuid"] + "1"}'
-    )
+def test_retrieve_player_view_with_wrong_uuid_returns_404(profile_data, api_rf):
+    random_uuid = str(uuid.uuid4())
+    request = api_rf.get(f"/api/v1/players/{random_uuid}/")
+    response = PlayerRetrieveAPIView.as_view()(request, uuid=f"{random_uuid}")
 
     assert response.status_code == 404
