9b69ad51c7efeefb4f7b15d4427cf3c87fb31b97
Martyna Witkowska <mar.witkowska@gmail.com>
2023-04-18 09:48:14 +0200

    AQ-79 badge list view

--- quiz/backend/stats/views.py
+++ quiz/backend/stats/views.py
@@ -7,3 +7,8 @@ from . import serializers, models
 class BadgeRetrieveView(generics.RetrieveAPIView):
     queryset = models.Badge.objects.all()
     serializer_class = serializers.BadgeSerializer
+
+
+class BadgeListView(generics.ListAPIView):
+    queryset = models.Badge.objects.all()
+    serializer_class = serializers.BadgeSerializer
