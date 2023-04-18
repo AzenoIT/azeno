7a90a4ef8b8d22709ace1660b80946bcbb8bc66b
Martyna Witkowska <mar.witkowska@gmail.com>
2023-04-18 10:34:41 +0200

    AQ-79 example data for badges, change order of serializer fields

--- quiz/backend/stats/serializers.py
+++ quiz/backend/stats/serializers.py
@@ -4,15 +4,17 @@ from . import models
 
 
 class BadgeSerializer(serializers.ModelSerializer):
+    image_url = serializers.URLField(source="image", read_only=True)
     badge_url = serializers.URLField(source="get_absolute_url", read_only=True)
 
     class Meta:
         model = models.Badge
         fields = (
+            "id",
+            "image_url",
             "name",
             "description",
             "owned_by_user_percentage",
             "points",
-            "image",
             "badge_url",
         )
