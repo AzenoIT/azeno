2e93eb0ad3702b98e736c47dcb899cc906b1eecc
Martyna Witkowska <92261595+martynawitkowska@users.noreply.github.com>
2023-04-20 13:43:13 +0200

    AQ-79 badge endpoint (#65)

---
+++ quiz/backend/stats/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+
+from . import models
+
+
+class BadgeSerializer(serializers.ModelSerializer):
+    """A serializer for :class:`stats.models.Badge`.
+
+    This serializer includes following fields:
+
+    * id: The unique identifier of the badge.
+    * image_url: The URL of the badge's image.
+    * name: The name of the badge.
+    * description: The description of the badge.
+    * owned_by_user_percentage: The percentage of users who own the badge.
+    * points: The number of points the badge is worth.
+    * badge_url: The absolute URL of the badge's detail view.
+
+    :ivar image_url: The URL field for image associated with badge instance.
+    :vartype image_url: serializers.URLField
+    :ivar badge_url: The URL field for the absolute url of the badge's detail view.
+    :vartype badge_url: serializers.URLField
+
+    """
+
+    image_url = serializers.URLField(source="image", read_only=True)
+    badge_url = serializers.URLField(source="get_absolute_url", read_only=True)
+
+    class Meta:
+        model = models.Badge
+        fields = (
+            "id",
+            "image_url",
+            "name",
+            "description",
+            "owned_by_user_percentage",
+            "points",
+            "badge_url",
+        )
---
+++ quiz/backend/stats/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = "stats"
+
+urlpatterns = [
+    path("badges/", views.BadgeListView.as_view(), name="badge_list"),
+    path("badges/<int:pk>/", views.BadgeRetrieveView.as_view(), name="badge_detail"),
+]
