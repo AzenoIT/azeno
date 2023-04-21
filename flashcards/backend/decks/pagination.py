6590ae4c8d07383bd0ef2c8d0c45060aeced36c9
Marcin <gonzur75@gmail.com>
2023-04-21 20:41:52 +0200

    fix

--- flashcards/backend/decks/pagination.py
+++ flashcards/backend/decks/pagination.py
@@ -2,4 +2,4 @@ from rest_framework import pagination
 
 
 class DecksPagination(pagination.LimitOffsetPagination):
-    max_page_size = 100
+    max_limit = 100
--- flashcards/backend/decks/tests/test_pagination.py
+++ flashcards/backend/decks/tests/test_pagination.py
@@ -3,13 +3,13 @@ from decks.pagination import DecksPagination
 from decks.viewsets import DeckViewSet
 
 
-def test_deck_list_pagination(api_rf, db):
+def test_deck_list_pagination(api_request_factory, db):
     factories.DeckFactory.create_batch(DecksPagination.max_limit + 1)
 
     url = f"/api/v1/decks/?limit={DecksPagination.max_limit + 1}&offset=0"
     view = DeckViewSet.as_view({"get": "list"})
 
-    request = api_rf.get(url)
+    request = api_request_factory.get(url)
     response = view(request)
 
-    assert len(response.data["results"]) == DecksPagination.max_limit
+    assert response.data["results"] == DecksPagination.max_limit
