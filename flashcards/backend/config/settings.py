b373e1ad84877cb3b6197f22e29d2e6f950f2793
BlazejBielski <Blazej Bielski>
2023-04-23 12:49:12 +0200

    install django filters module end add to installed apps

--- flashcards/backend/config/settings.py
+++ flashcards/backend/config/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
+    'django_filters',
     # Local
     "users.apps.UsersConfig",
     "decks.apps.DecksConfig",
