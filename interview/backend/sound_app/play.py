298e61d0dedc7295eefc97fd193dca0726b19c27
Bartosz Szymik <szymik.bartek@gmail.com>
2023-04-24 23:51:27 +0200

    instalation pyaudio, integration to django, needs to be fixed

--- interview/backend/sound_app/play.py
+++ interview/backend/sound_app/play.py
@@ -3,8 +3,5 @@ from pydub.playback import play
 
 
 def play_file(filename):
-    ffmpeg_path = '/opt/homebrew/bin/ffmpeg'
-    AudioSegment.ffmpeg = ffmpeg_path
-
     sound = AudioSegment.from_wav(filename)
     play(sound)
