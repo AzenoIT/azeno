a5efb437002eb136de23002f5e65d0d5da58f2a1
Blazej Bielski <bbielski@lyck.pl>
2023-04-27 09:22:30 +0200

    black formated

--- flashcards/backend/config/settings.py
+++ flashcards/backend/config/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
-    'django_filters',
+    "django_filters",
     # Local
     "users.apps.UsersConfig",
     "decks.apps.DecksConfig",
