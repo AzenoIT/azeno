2f779efc2192ad21efb50cedc28997bc59d4b252
Krzysiek <krzysztof.plonka64@gmail.com>
2023-04-29 13:21:55 +0200

    AQ-85 Configured Avatar.tsx further

--- bank/src/components/Avatar/Avatar.stories.tsx
+++ bank/src/components/Avatar/Avatar.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryFn } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import AvatarComponent from "@azeno/bank/components/Avatar/Avatar";
 import { ComponentProps } from "react";
 
@@ -10,6 +10,6 @@ export default {
 } satisfies Meta<typeof AvatarComponent>;
 
 type Props = ComponentProps<typeof AvatarComponent>;
-export const Avatar: StoryFn<typeof AvatarComponent> = ({ src, alt, width, height }: Props) => (
-    <AvatarComponent src={src} alt={alt} width={width} height={height} />
-);
+export const Avatar: StoryObj<typeof AvatarComponent> = {
+    render: ({ src, alt, type }: Props) => <AvatarComponent src={src} alt={alt} type={type} />,
+};
--- bank/src/components/Avatar/Avatar.tsx
+++ bank/src/components/Avatar/Avatar.tsx
@@ -1,12 +1,20 @@
 import { Avatar as AvatarMUI } from "@mui/material";
 
+type AvatarTypes = "sm" | "md" | "lg";
+
+const typeConfig: { [key in AvatarTypes]: string } = {
+    sm: "w-[50px] h-[50px] border-3 border-tertiary-800",
+    md: "w-[70px] h-[70px] border-5 border-tertiary-800",
+    lg: "w-[140px] h-[140px] border-10 border-tertiary-800",
+};
+
 interface AvatarProps {
     src?: string;
     alt?: string;
-    width?: number;
-    height?: number;
+    type?: AvatarTypes;
 }
 
-export default function Avatar({ src, alt = "Avatar Picture", width = 140, height = 140 }: AvatarProps) {
-    return <AvatarMUI src={src} alt={alt} sx={{ width, height }} className="border-[10px] border-tertiary-800" />;
+export default function Avatar({ src, alt = "Avatar Picture", type = "md" }: AvatarProps) {
+    const config = typeConfig[type];
+    return <AvatarMUI src={src} alt={alt} className={config} />;
 }
